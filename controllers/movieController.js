e8e7a8cc3f6c0a8b76ea50094390ad236a750f5e
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-01-23 20:59:22 +0100

    feat: connessione al server + rotte

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,14 +1,65 @@
+const connection = require("../data/dbConnection")
 
 //index
-const index = (req,res,next) =>{
+const index = (req, res, next) => {
 
-    res.json("sono index");
-    
+    const sql = "SELECT * FROM movies";
+
+    connection.query(sql, (err, movies) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Errore interno del server",
+            });
+        } else {
+            return res.status(200).json({
+                status: "success",
+                data: movies,
+            });
+        }
+    });
 };
 
-const show = (req,res,next) =>{
-    res.json("sono show");
-    
+const show = (req, res, next) => {
+    const id = req.params.id;
+
+    const sql = "SELECT * FROM movies WHERE id = ?";
+    const sqlReviews = `
+    SELECT reviews.* 
+    FROM reviews
+    JOIN movies
+    ON movies.id = reviews.movie_id
+    WHERE movies.id = ?
+  `;
+
+
+    connection.query(sql, [id], (err, movies) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Errore interno del server",
+            });
+        } else if (movies.length === 0) {
+            return res.status(404).json({
+                message: "Film non trovato",
+            })
+        }
+        else {
+            connection.query(sqlReviews, [id], (err, reviews) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Errore interno del server",
+                    });
+                } else {
+                    return res.status(200).json({
+                        status: "success",
+                        data: {
+                            ...movies[0],
+                            reviews
+                        },
+                    });
+                }
+            });
+        }
+    });
 }
 
 module.exports = {
